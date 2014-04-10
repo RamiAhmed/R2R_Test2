e6444851835bf88838cb68bdc0388e0f4770cb2d
Rami Ahmed Bock <rami@alphastagestudios.com>
2014-04-10 11:19:00 +0200

    Added eye tracker listener code
    - Also removed GA GUI

--- Assets/Scripts/MouseTracker.cs
+++ Assets/Scripts/MouseTracker.cs
@@ -9,16 +9,21 @@ public class MouseTracker : MonoBehaviour {
 	private GameController _gameControllerRef = null;
 	private Camera playerCamRef = null;
 
+	private EyeTribeClient eyeClient = null;
+
 	// Use this for initialization
 	void Start () {
 		_gameControllerRef = this.GetComponent<GameController>();
-
+		eyeClient = GameObject.FindGameObjectWithTag ("EyeTribeHandler").GetComponent<EyeTribeClient>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (playerCamRef == null) 
 			playerCamRef = _gameControllerRef.players[0].PlayerCam;
+
+		if (eyeClient == null) 
+			eyeClient = GameObject.FindGameObjectWithTag("EyeTribeHandler").GetComponent<EyeTribeClient>();
 
 		if (Time.time - lastMouse > 1f/MouseTrackFrequencyPerSecond) {
 			lastMouse = Time.time;
@@ -30,25 +35,29 @@ public class MouseTracker : MonoBehaviour {
 	}
 
 	private void trackEyes(string timeNow) {
-		/*
-		Vector2 eyesPos = Vector2.zero;
-		Vector3 eyesPos2D = new Vector3(eyesPos.x, eyesPos.y, 0f);
-		GA.API.Design.NewEvent("EyesPosition2D:" + timeNow, eyesPos2D);
+		if (eyeClient == null)
+			return;
 
-		if (playerCamRef != null) {
-			Ray eyesRay = playerCamRef.ScreenPointToRay(eyesPos2D);
-			RaycastHit[] hits = Physics.RaycastAll(eyesRay);
-			Vector3 eyesPos3D = Vector3.zero;
-			foreach (RaycastHit hit in hits) {
-				if (hit.collider.GetType() == typeof(TerrainCollider)) {
-					eyesPos3D = new Vector3(hit.point.x, hit.point.y, hit.point.z);
-					break;
+		Vector3 currentGaze = eyeClient.gazePosInvertY;
+		if (currentGaze.z > 0) {
+			Vector2 eyesPos = new Vector2(currentGaze.x, currentGaze.y);
+			Vector3 eyesPos2D = new Vector3 (eyesPos.x, eyesPos.y, 0f);
+			GA.API.Design.NewEvent ("EyesPosition2D:" + timeNow, eyesPos2D);
+
+			if (playerCamRef != null) {
+				Ray eyesRay = playerCamRef.ScreenPointToRay (eyesPos2D);
+				RaycastHit[] hits = Physics.RaycastAll (eyesRay);
+				Vector3 eyesPos3D = Vector3.zero;
+				foreach (RaycastHit hit in hits) {
+					if (hit.collider.GetType () == typeof(TerrainCollider)) {
+							eyesPos3D = new Vector3 (hit.point.x, hit.point.y, hit.point.z);
+							break;
+					}
 				}
-			}
 
-			GA.API.Design.NewEvent("EyesPosition3D:" + timeNow, eyesPos3D);
+				GA.API.Design.NewEvent ("EyesPosition3D:" + timeNow, eyesPos3D);
+			}
 		}
-		*/
 	}
 
 	private void trackMouse(string timeNow) {
